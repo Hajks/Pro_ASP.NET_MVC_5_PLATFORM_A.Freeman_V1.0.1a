05bfa29ab45379a18e098d836fdbe4089ef19d89
Hajk <tunczorx@gmail.com>
2018-11-08 13:23:17 +0100

    Add project files.

---
+++ Pro_ASP.NET_MVC_5_PLATFORM_A.Freeman_V1.0.1a.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pro_ASP.NET_MVC_5_PLATFORM_A.Freeman_V1._0._1a.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
